022a3a8c42ef18048d20366d3e158d8c2f28ea9e
damanjot6767 <damanjot6767@gmail.com>
2023-01-21 15:56:56 +0530

    add in it

--- src/connect/connect.js
+++ src/connect/connect.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config()
 
 const connect = async()=>{
-   return new mongoose.connect(`${process.env.MONGOURL}`)
+   return new mongoose.connect(`mongodb+srv://damanjot6767:damanjot6767@cluster0.5qggddx.mongodb.net/calculate?retryWrites=true&w=majority`)
 }
 module.exports=connect;
